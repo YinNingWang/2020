dcce0bdd290afd985656d84810d1bff46dc9374d
YinNingWang <ej2010eg@gmail.com>
2020-06-14 16:52:27 +0800

    initial my old resume web 2018
    初次部署網站至Github Pages

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: '2JwtRRzl5qM', start: 3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
